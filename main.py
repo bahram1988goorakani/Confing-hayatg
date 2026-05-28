effb6546b133428ef7b73eda1d3d43ec7934ae38
bahram1988goorakani <bahram1988goorakani@gmail.com>
2026-05-29 01:59:47 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import re
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+session = os.getenv("STRING_SESSION")
+target = os.getenv("TARGET_CHANNEL")
+
+channels = [
+    "channel1",
+    "channel2"
+]
+
+patterns = [
+    r"vmess://[^\s]+",
+    r"vless://[^\s]+",
+    r"trojan://[^\s]+",
+    r"ss://[^\s]+"
+]
+
+client = TelegramClient(
+    StringSession(session),
+    api_id,
+    api_hash
+)
+
+async def main():
+    for ch in channels:
+        async for msg in client.iter_messages(ch, limit=20):
+            if msg.message:
+                for p in patterns:
+                    for config in re.findall(p, msg.message):
+                        await client.send_message(target, config)
+
+with client:
+    client.loop.run_until_complete(main())
